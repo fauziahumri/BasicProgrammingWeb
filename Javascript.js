beaa6a7e560521615dd0c5ef1ee223ef25534aec
fauziahumri <96508690+fauziahumri@users.noreply.github.com>
2023-06-13 21:29:48 +0700

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+    });
+    $("#flip").click(function(){
+    $("#flip").hide();
+    });
+
+    $("#flip1").click(function(){
+    $("#panel1").slideDown("slow");
+    });
+    $("#flip1").click(function(){
+    $("#flip1").hide();
+    });
+
+    $("#flip2").click(function(){
+    $("#panel2").slideDown("slow");
+    });
+    $("#flip2").click(function(){
+    $("#flip2").hide();
+    });
+});
